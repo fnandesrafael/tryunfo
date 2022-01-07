2eeccc7c984a89ab0d07018af43aae7516275d80
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-01-07 17:44:17 -0300

    :sparkles: Requisito 0
    Adiciona a construçao e renderizaçao da carta, de acordo com os valores passados nas props(ainda nao personalizavel)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   constructor() {
@@ -34,6 +35,16 @@ class App extends React.Component {
           isSaveButtonDisabled={ false }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo
         />
       </div>
     );
---
+++ src/components/Card.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Card extends Component {
+  render() {
+    const
+      {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+        cardTrunfo,
+      } = this.props;
+    return (
+      <div>
+        <h3 data-testid="name-card">{ cardName }</h3>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <div>
+          <h5 data-testid="attr1-card">{ cardAttr1 }</h5>
+          <h5 data-testid="attr2-card">{ cardAttr2 }</h5>
+          <h5 data-testid="attr3-card">{ cardAttr3 }</h5>
+        </div>
+        <h4 data-testid="rare-card">{ cardRare }</h4>
+        {cardTrunfo === true ? (
+          <h3 data-testid="trunfo-card">Super Trunfo</h3>
+        ) : null}
+        <p data-testid="description-card">{ cardDescription }</p>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
