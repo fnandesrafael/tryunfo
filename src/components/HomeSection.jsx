33c70a04cf70aa9cbd39f1388e9d9287aa9013ae
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-10-01 16:06:23 -0300

    feat: implementação inicial dos novos componentes

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomeSection() {
+  return (
+    <>
+      <h1>Pokétrunfo</h1>
+      <p>Crie suas cartas, monte seu baralho e divirta-se!</p>
+    </>
+  );
+}
